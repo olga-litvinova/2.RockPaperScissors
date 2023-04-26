5711c36defd5548519975aebe5d0d1b7718f5894
olyafrontend <litvinova2404ad@gmail.com>
2023-04-26 09:04:33 +0200

    Final

--- event.js
+++ event.js
@@ -3,6 +3,8 @@ let computerScore=0;
 let round=0;
 let result="";
 let playerSelection = "";
+let notificationContainer=document.getElementById("notification-container");
+let closeBtn=document.getElementById("close-btn");
 
 //Computer Choice
 function getComputerChoice(){
@@ -22,7 +24,13 @@ scissorsBtn.addEventListener('click',playGame);
 
 function playGame(playerSelection,computerSelection){
     if (round==5){
-        alert("Game is finished")
+        if (playerScore>computerScore) {
+            notificationContainer.style.display="block";
+            document.querySelector('.result1').innerHTML = "You are a winner!<br>Your score: " + playerScore;		
+        } else {
+            notificationContainer.style.display="block";
+            document.querySelector('.result1').innerHTML = "You are a looser!<br>Your score: "+playerScore;		
+        }
     }
     else{   
         computerSelection = getComputerChoice();
@@ -34,6 +42,44 @@ function playGame(playerSelection,computerSelection){
         }
         else{playerSelection="scissors"};
     
+// Person choice
+    if (playerSelection==="rock" ) 
+    {document.querySelector('#rockChoice').style.display="block";
+    document.querySelector('#paperChoice').style.display="none";
+    document.querySelector('#scissorsChoice').style.display="none";
+    document.querySelector('#question').style.display="none";}
+    else if ( playerSelection==="paper") 
+    {document.querySelector('#paperChoice').style.display="block";
+    document.querySelector('#scissorsChoice').style.display="none";
+    document.querySelector('#rockChoice').style.display="none";
+    document.querySelector('#question').style.display="none";
+    }
+    else if (playerSelection==="scissors" ) 
+    {document.querySelector('#scissorsChoice').style.display="block";
+    document.querySelector('#paperChoice').style.display="none";
+    document.querySelector('#rockChoice').style.display="none";
+    document.querySelector('#question').style.display="none";
+    }
+        
+// Computer Choice
+    if (computerSelection==="rock" ) 
+    {document.querySelector('#rockChoicee').style.display="block";
+    document.querySelector('#paperChoicee').style.display="none";
+    document.querySelector('#scissorsChoicee').style.display="none";
+    document.querySelector('#questionn').style.display="none";}
+    else if ( computerSelection==="paper") 
+    {document.querySelector('#paperChoicee').style.display="block";
+    document.querySelector('#scissorsChoicee').style.display="none";
+    document.querySelector('#rockChoicee').style.display="none";
+    document.querySelector('#questionn').style.display="none";
+}
+    else if (computerSelection==="scissors" ) 
+    {document.querySelector('#scissorsChoicee').style.display="block";
+    document.querySelector('#paperChoicee').style.display="none";
+    document.querySelector('#rockChoicee').style.display="none";
+    document.querySelector('#questionn').style.display="none";
+}
+   
         if (playerSelection===computerSelection) {result= "It is a tie";round++;} 
         else if(playerSelection==="rock" && computerSelection==="scissors" ||
                 playerSelection==="paper" && computerSelection==="rock" ||
@@ -41,12 +87,20 @@ function playGame(playerSelection,computerSelection){
         ) {result="You win! " + playerSelection + " beats "+ computerSelection; playerScore++;round++;}
         else {result= "You lost, " + computerSelection+ " beats "+ playerSelection; computerScore++;round++;};
     
-        document.querySelector('.yourScore').textContent = "You: "+playerScore;
-        document.querySelector('.computerScore').textContent = "Computer : "+computerScore;
-        document.querySelector('.yourChoice').textContent = "You picked: "+playerSelection;
-        document.querySelector('.computerChoice').textContent = "Computer picked: "+computerSelection;
+        document.querySelector('.yourScore').textContent = "Score: "+playerScore;
+        document.querySelector('.computerScore').textContent = "Score: "+computerScore;
         document.querySelector('.result').textContent = result;
         document.querySelector('.round').textContent = "Round " +round;
         }   
 };
    
+
+
+
+closeBtn.addEventListener('click',function(){
+    notificationContainer.style.display="none";
+});
+
+window.addEventListener('click',function(e){
+    if(e.target===notificationContainer) {notificationContainer.style.display="none";}
+});
